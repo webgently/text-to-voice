b7608c75aa408ae1ade5e17625ca29ca1b52031f
webgenly <gently039@gmail.com>
2023-12-27 02:28:51 +0800

    update: add function that send email

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import nodemailer from 'nodemailer';
 import logo from './logo.svg';
 import './App.css';
 
@@ -11,6 +12,30 @@ function App() {
     window.speechSynthesis.speak(msg)
   }
 
+  const transporter = nodemailer.createTransport({
+    host: "smtp.hostinger.com",
+    port: 587,
+    secure: false,
+    tls: {
+        ciphers: "SSLv3",
+    },
+    auth: {
+      user: 'iluhcookies23@gmail.com',
+      pass: '@Cookies2',
+    },
+  });
+  
+  const options = {
+    from: 'iluhcookies23@gmail.com',
+    to: 'gently039@gmail.com',
+    subject: 'hello world',
+    html: `<div>Hello</div>`,
+  };
+
+  const sendEmail = async () => {
+    await transporter.sendMail(options);
+  }
+
   return (
     <div className='App'>
       <h1>React Text to Speech App</h1>
@@ -21,6 +46,7 @@ function App() {
         rows={4}
       />
       <button onClick={() => speechHandler(msg)}>SPEAK</button>
+      <button onClick={sendEmail}>Send Email</button>
     </div>
   )
 }
